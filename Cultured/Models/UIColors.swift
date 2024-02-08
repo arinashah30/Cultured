9f93e0eef592e687c6a9996c05dfb4f86de182b0
ryan-omeara <ryanomeara12@gmail.com>
2024-02-08 15:11:29 -0500

    Added fonts, current colors, and an example UI button in home page

---
+++ Cultured/Models/UIColors.swift
@@ -0,0 +1,36 @@
+//
+//  UIColors.swift
+//  Cultured
+//
+//  Created by Ryan O’Meara on 2/8/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let darkMode = false
+    
+    public static var cBlue: Color {
+        Color(red:0, green:102, blue:176)
+    }
+    public static var cGreen: Color {
+        Color(red:45, green:198, blue:83)
+    }
+    public static var cOrange: Color {
+        Color(red:254, green:165, blue:30)
+    }
+    public static var cRed: Color {
+        Color(red:247, green:64, blue:64)
+    }
+    public static var cDarkGray: Color {
+        Color(red:64, green:64, blue:64)
+    }
+    public static var cMedGray: Color {
+        Color(red:148, green:148, blue:148)
+    }
+    public static var cLightGray: Color {
+        Color(red:246, green:246, blue:246)
+    }
+    
+}
