edd06be5b8ff629c8bc8d5c306cc5e85273365a1
Connor-1233 <Connor.e.case@gmail.com>
2024-02-08 19:36:01 -0500

    Adding Error for SignUp

--- Cultured/ViewModels/ViewModel.swift
+++ Cultured/ViewModels/ViewModel.swift
@@ -13,4 +13,27 @@ import FirebaseStorage
 
 class ViewModel: ObservableObject {
     @Published var current_user: User? = nil
+    
+    let db = Firestore.firestore()
+    let auth = Auth.auth()
+    
+    @Published var errorText: String? = nil
+    
+    func firebase_email_password_sign_up_(email: String, password: String) {
+        auth.createUser(withEmail: email, password: password) { authResult, error in
+            if let errorSignUp = error {
+                let firebaseError = AuthErrorCode.Code(rawValue: errorSignUp._code)
+                switch firebaseError {
+                case .emailAlreadyInUse:
+                    self.errorText = "An account with this email already exists"
+                case .weakPassword:
+                    self.errorText = "This password is too weak"
+                default:
+                    self.errorText = "An error has occurred"
+                }
+            } else if let user = authResult?.user {
+                
+            }
+        }
+    }
 }
