d3a2ffc425cd45ca24d5201a155fb6e19db785e0
arinashah30 <ashah634@gatech.edu>
2024-02-13 14:43:52 -0500

    Added ARLandmark model and updated 3DModelView

---
+++ Cultured/Models/ARLandmark.swift
@@ -0,0 +1,16 @@
+//
+//  ARLandmark.swift
+//  Cultured
+//
+//  Created by Arina Shah on 2/13/24.
+//
+
+import Foundation
+import SwiftUI
+import RealityKit
+
+struct ARLandmark {
+    var modelName: String
+    var numFacts: Int
+    var color: UIColor? = nil
+}
--- Cultured/Views/3DModelView.swift
+++ Cultured/Views/3DModelView.swift
@@ -11,25 +11,27 @@ import ARKit
 
 struct _DModelView : View {
     var body: some View {
-        RealityViewContainer(modelName: "Eiffel_Tower", numFacts: 2).edgesIgnoringSafeArea(.all)
+        RealityViewContainer(model: ARLandmark(modelName: "Eiffel_Tower", numFacts: 2, color: .gray)).edgesIgnoringSafeArea(.all)
     }
 }
 
 struct RealityViewContainer: UIViewRepresentable {
     
-    var modelName: String
-    var numFacts: Int
+    var model: ARLandmark
     
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
-        guard let modelEntity = try? ModelEntity.loadModel(named: modelName) else {
+        guard let modelEntity = try? ModelEntity.loadModel(named: model.modelName) else {
             return arView
         }
         
-        var material = SimpleMaterial()
-        material.color = .init(tint: .gray)
-        modelEntity.model?.materials[0] = material
+        if let color = model.color {
+            var material = SimpleMaterial()
+            material.color = .init(tint: color)
+            modelEntity.model?.materials[0] = material
+        }
+        
         
         modelEntity.scale = [0.1, 0.1, 0.1]
         
@@ -37,7 +39,7 @@ struct RealityViewContainer: UIViewRepresentable {
         let anchor = AnchorEntity(.plane(.horizontal, classification: .floor, minimumBounds: [0.5, 0.5]))
         anchor.addChild(modelEntity)
         modelEntity.position = [0, 0, -10]
-        for i in 0..<numFacts {
+        for i in 0..<model.numFacts {
             var informationBubbleEntity = ModelEntity(mesh: MeshResource.generateSphere(radius: 0.5), materials: [SimpleMaterial(color: .red, isMetallic: true)])
             let relativeTransform = Transform(translation: [Float(i) + 1, Float(i) + 1, Float(i) + 1])
             informationBubbleEntity.transform = relativeTransform
