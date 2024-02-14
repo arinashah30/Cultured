11faba19b86b27f8e5c34467f433fd2c09ff78ae
jifonthespoon <austin.huguenard@gmail.com>
2024-02-13 19:36:06 -0500

    Add functions to quizzes

--- Cultured/ViewModels/QuizViewModel.swift
+++ Cultured/ViewModels/QuizViewModel.swift
@@ -5,10 +5,31 @@
 //  Created by Austin Huguenard on 2/13/24.
 //
 
+//get_current_question
+//get_answer_choices
+//get_next_question
+//submit_answer
+
 import Foundation
 
 class QuizViewModel: ObservableObject {
     @Published var current_user: User? = nil
     @Published var current_quiz: Quiz? = nil
     
+    func get_answer_choices() -> [String] {
+        return (current_quiz?.questions[current_quiz!.currentQuestion].answers)!
+    }
+    
+    func submit_answer(selectedAnswer: String) {
+        var currentQuestion = current_quiz!.questions[current_quiz!.currentQuestion]
+        if selectedAnswer == currentQuestion.answers[currentQuestion.correctAnswer] {
+            current_quiz?.points += 10
+        }
+        currentQuestion.submitted = true
+        UserDefaults.setValue(current_quiz!.currentQuestion + 1, forKey: "currentQuizQuestion")
+    }
+    
+    func finish_quiz() {
+        // add points function from FB
+    }
 }
