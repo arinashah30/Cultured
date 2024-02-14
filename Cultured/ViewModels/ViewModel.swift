ca744077899233c88c6277d9ee8ec00d0d1ac604
Younis, Lana <lyounis6@gatech.edu>
2024-02-13 19:38:04 -0500

    get points and badges func

--- Cultured/ViewModels/ViewModel.swift
+++ Cultured/ViewModels/ViewModel.swift
@@ -13,4 +13,42 @@ import FirebaseStorage
 
 class ViewModel: ObservableObject {
     @Published var current_user: User? = nil
+    
+    let db = Firestore.firestore()
+    let auth = Auth.auth()
+
+    func getPts(userID: String, completion: @escaping (Int) -> Void) {
+        db.collection("USERS").document(userID).getDocument { document, error in
+            if let err = error {
+                print(err.localizedDescription)
+                return
+            } else {
+                if let doc = document {
+                    if let data = doc.data() {
+                        let points = data["points"] as? Int
+                        completion(points!)
+                    }
+                }
+            }
+        }
+    }
+    
+    func getBadges(userID: String, completion: @escaping ([String]) -> Void) {
+        db.collection("USERS").document(userID).getDocument { document, error in
+            if let err = error {
+                print(err.localizedDescription)
+                return
+            } else {
+                if let doc = document {
+                    if let data = doc.data() {
+                        let badges = data["badges"] as? [String]
+                        completion(badges!)
+                    }
+                }
+            }
+        }
+    }
 }
+
+
+
