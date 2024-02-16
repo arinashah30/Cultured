2c1e540aeae0056c164ec272b9f4f9a461456272
gandenfunggt <gandenfunggt@gmail.com>
2024-02-15 19:25:06 -0500

    added addBadge functionality alongside the signIn function

--- Cultured/ViewModels/ViewModel.swift
+++ Cultured/ViewModels/ViewModel.swift
@@ -13,6 +13,9 @@ import FirebaseStorage
 
 class ViewModel: ObservableObject {
     @Published var current_user: User? = nil
+    let db = Firestore.firestore()
+    let auth = Auth.auth()
+
     
     func fireBaseSignIn(email: String, password: String, completion: @escaping (Bool) -> Void) {
         Auth.auth().signIn(withEmail: email, password: password) {  authResult, error in
@@ -25,4 +28,44 @@ class ViewModel: ObservableObject {
             //doesn't handle the case where authResult is nil so write that in if needed
         }
     }
+    
+    func addBadges(userID: String, newBadge: String, completion: @escaping (Bool) -> Void) {
+        db.collection("USERS").document(userID).getDocument { [self] document, error in
+            if let err = error {
+                print(err.localizedDescription)
+                completion(false)
+                return
+            }
+            
+            guard let doc = document else {
+                print("Not able to access the document")
+                completion(false)
+                return
+            }
+            
+            guard var data = doc.data() else {
+                print("Document has no data")
+                completion(false)
+                return
+            }
+            
+            if var badges = data["badges"] as? [String] {
+                badges.append(newBadge)
+                data["badges"] = badges
+                
+                db.collection("USERS").document(userID).updateData(["badges": badges]) { error in
+                    if let err = error {
+                        print(err.localizedDescription)
+                        completion(false)
+                    } else {
+                        completion(true)
+                    }
+                }
+            } else {
+                print("No existing badges found")
+                completion(false)
+            }
+        }
+    }
+
 }
