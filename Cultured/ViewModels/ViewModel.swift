4fcce8858fb477772c22702004adaae615f5f325
Younis, Lana <lyounis6@gatech.edu>
2024-02-20 19:34:10 -0500

    getModuleinfo func

--- Cultured/ViewModels/ViewModel.swift
+++ Cultured/ViewModels/ViewModel.swift
@@ -23,41 +23,80 @@ class ViewModel: ObservableObject {
                 completion(true)
             }
             //doesn't handle the case where authResult is nil so write that in if needed
-    let db = Firestore.firestore()
-    let auth = Auth.auth()
-
-    func getPts(userID: String, completion: @escaping (Int) -> Void) {
-        db.collection("USERS").document(userID).getDocument { document, error in
-            if let err = error {
-                print(err.localizedDescription)
-                return
-            } else {
-                if let doc = document {
-                    if let data = doc.data() {
-                        let points = data["points"] as? Int
-                        completion(points!)
+            let db = Firestore.firestore()
+            let auth = Auth.auth()
+            
+            func getPts(userID: String, completion: @escaping (Int) -> Void) {
+                db.collection("USERS").document(userID).getDocument { document, error in
+                    if let err = error {
+                        print(err.localizedDescription)
+                        return
+                    } else {
+                        if let doc = document {
+                            if let data = doc.data() {
+                                let points = data["points"] as? Int
+                                completion(points!)
+                            }
+                        }
                     }
                 }
             }
-        }
-    }
-    
-    func getBadges(userID: String, completion: @escaping ([String]) -> Void) {
-        db.collection("USERS").document(userID).getDocument { document, error in
-            if let err = error {
-                print(err.localizedDescription)
-                return
-            } else {
-                if let doc = document {
-                    if let data = doc.data() {
-                        let badges = data["badges"] as? [String]
-                        completion(badges!)
+            
+            func getBadges(userID: String, completion: @escaping ([String]) -> Void) {
+                db.collection("USERS").document(userID).getDocument { document, error in
+                    if let err = error {
+                        print(err.localizedDescription)
+                        return
+                    } else {
+                        if let doc = document {
+                            if let data = doc.data() {
+                                let badges = data["badges"] as? [String]
+                                completion(badges!)
+                            }
+                        }
                     }
                 }
+            }
+            
+            func getInfoFromModule(country: String, module: String, completion: @escaping (String?) -> Void) {
+                db.collection("COUNTRIES").document(country).getDocument {(doc, error) in
+                    if let err = error {
+                        print(err.localizedDescription)
+                        completion(nil)
+                        return
+                    }
+                    
+                    guard let document = doc, document.exists else {
+                        print("no doc")
+                        completion(nil)
+                        return
+                    }
+                    
+                    let modules = document.reference.collection("MODULES")
+                    
+                    modules.document(module).getDocument {(docu, e) in
+                        if let e = e {
+                            print(e.localizedDescription)
+                            completion(nil)
+                            return
+                        }
+                        guard let modsdoc = docu, modsdoc.exists else {
+                            print("no doc")
+                            completion(nil)
+                            return
+                        }
+                        
+                        let data: [String: Any]? = modsdoc.data()
+                        
+                        let moduleInfo: String? = data?[module] as? String
+                        completion(moduleInfo)
+                        
+                    }
+                    
+                }
+                
             }
         }
     }
 }
-
-
 
