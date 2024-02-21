67a9acdda6613f3f8538d3dfc8373689f1f81fc3
royankit11 <royankit11@gmail.com>
2024-02-20 19:09:35 -0500

    Google Service

--- Cultured/ViewModels/ViewModel.swift
+++ Cultured/ViewModels/ViewModel.swift
@@ -13,6 +13,8 @@ import FirebaseStorage
 
 class ViewModel: ObservableObject {
     @Published var current_user: User? = nil
+    let db = Firestore.firestore();
+    let auth = Auth.auth();
     
     func fireBaseSignIn(email: String, password: String, completion: @escaping (Bool) -> Void) {
         Auth.auth().signIn(withEmail: email, password: password) {  authResult, error in
@@ -25,4 +27,28 @@ class ViewModel: ObservableObject {
             //doesn't handle the case where authResult is nil so write that in if needed
         }
     }
+    
+    func createNewCountry(countryName: String) {
+                let countryRef = db.collection("COUNTRIES").document(countryName)
+                countryRef.setData(["population": 5000])
+                
+                let modules = [
+                        "MUSIC",
+                        "CELEBRITIES",
+                        "ETIQUETTE",
+                        "FOOD",
+                        "HOLIDAYS",
+                        "LANDMARKS",
+                        "MAJORCITIES",
+                        "SPORTS",
+                        "TRADITIONS",
+                        "TVMOVIE"
+                    ]
+
+                    for module in modules {
+                        // For each module, create a new document in the "MODULES" subcollection
+                        countryRef.collection("MODULES").document(module).setData(["someData": "value"])
+                    }
+                
+            }
 }
