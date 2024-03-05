b8d3fbab201bf81ee21ed883e83cc8d646ea4a3c
sameerarora1 <saroragt@gmail.com>
2024-03-04 23:31:49 -0500

    New Files for Connections View

--- Cultured/ViewModels/ConnectionsViewModel.swift
+++ Cultured/ViewModels/ConnectionsViewModel.swift
@@ -11,4 +11,24 @@ class ConnectionsViewModel: ObservableObject {
     @Published var current_user: User? = nil
     @Published var current_connections_game: Connections? = nil
     
+    func playConnections() {
+        
+    }
+
+    
+    //METHOD IN PROGRESS
+//    func getCategoryInfo(index: Int) -> [String] {
+//        var infoArray : [String] = []
+//        var category : String = Connections.categories[index]
+//        
+//        infoArray.append(category)
+//        
+//        var wordsOfCategory = Connections.answerKey[category]
+//        
+//        for word in wordsOfCategory {
+//            infoArray.append(word)
+//        }
+//        
+//        return infoArray
+//    }
 }
