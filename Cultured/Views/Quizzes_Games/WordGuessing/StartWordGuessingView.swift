b8112a57d570fd2f2e592ab55448a344fdaa8ae6
shreyasgoyal213 <the.shreyasgoyal@gmail.com>
2024-03-04 20:42:07 -0500

    Navigation back to game start screen from main game screen.

--- Cultured/Views/Quizzes_Games/WordGuessing/StartWordGuessingView.swift
+++ Cultured/Views/Quizzes_Games/WordGuessing/StartWordGuessingView.swift
@@ -127,6 +127,7 @@ struct StartWordGuessingView: View {
                 }
             }
         }
+        .navigationBarBackButtonHidden()
     }
 }
 
--- Cultured/Views/Quizzes_Games/WordGuessing/WordGuessingView.swift
+++ Cultured/Views/Quizzes_Games/WordGuessing/WordGuessingView.swift
@@ -15,112 +15,115 @@ struct WordGuessingView: View {
     let colors: [Color] = [Color("Gradient1"), Color("Gradient2"), Color("Gradient3"), Color("Gradient4"), Color("Gradient5"), Color("Gradient6"), Color("Gradient7"), Color("Gradient8"), Color("Gradient9")]
         
     var body: some View {
-        VStack {
-            if let game = vm.current_word_guessing_game {
-                HStack {
-                    Button {
-                    } label: {
-                        ZStack {
-                            Circle()
-                                .frame(width: 50, height: 50)
-                                .padding(.top, 5)
-                                .padding(.leading, 20)
-                                .foregroundColor(Color.black.opacity(0.1))
-                            Image("Arrow")
-                                .padding(.top, 5)
-                                .padding(.leading, 18)
+        NavigationStack {
+            VStack {
+                if let game = vm.current_word_guessing_game {
+                    HStack {
+                        NavigationLink(destination: {
+                            StartWordGuessingView(vm: ViewModel())
+                                    }, label: {
+                            ZStack {
+                                Circle()
+                                    .frame(width: 50, height: 50)
+                                    .padding(.top, 5)
+                                    .padding(.leading, 20)
+                                    .foregroundColor(Color.black.opacity(0.1))
+                                Image("Arrow")
+                                    .padding(.top, 5)
+                                    .padding(.leading, 18)
+                            }
+                        })
+                        .padding(.trailing, 300)
+                    }
+                    Spacer(minLength: 15)
+                    HStack{
+                        Spacer(minLength: 20)
+                        Text("Hints")
+                            .font(Font.custom("Quicksand-Medium", size: 24))
+                        Spacer(minLength: 190)
+                        
+                        Button(action: {
+                            vm.flipTile()
+                        }) {
+                            Text("Next hint")
+                                .font(.title3)
+                                .foregroundColor(.gray)
+                                .font(Font.custom("SF-Pro-Display-Light", size: 18))
                         }
+                        Spacer(minLength: 20)
                     }
-                    .padding(.trailing, 300)
-                }
-                Spacer(minLength: 15)
-                HStack{
-                    Spacer(minLength: 20)
-                    Text("Hints")
-                        .font(Font.custom("Quicksand-Medium", size: 24))
-                    Spacer(minLength: 190)
                     
-                    Button(action: {
-                        vm.flipTile()
-                    }) {
-                        Text("Next hint")
-                            .font(.title3)
-                            .foregroundColor(.gray)
-                            .font(Font.custom("SF-Pro-Display-Light", size: 18))
-                    }
-                    Spacer(minLength: 20)
-                }
-                
-                ScrollView {
-                    VStack {
-                        ForEach(game.options.indices, id: \.self) { index in
-                            if (!game.options[index].isFlipped) {
-                                Text("")
-                                    .foregroundColor(.black)
-                                    .frame(width: 336 - CGFloat((23 * index)), height: 51)
-                                    .background(colors[index % colors.count])
-                                    .cornerRadius(10)
-                                    .padding(.horizontal)
-                                    .padding(.bottom, 10)
-                            } else {
-                                Text(game.options[index].option)
-                                    .font(Font.custom("SF-Pro-Display-Light", size: 19))
-                                    .foregroundColor(.black)
-                                    .frame(width: 336 - CGFloat((23 * index)), height: 51)
-                                    .background(colors[index % colors.count])
-                                    .cornerRadius(10)
-                                    .padding(.horizontal)
-                                    .padding(.bottom, 10)
+                    ScrollView {
+                        VStack {
+                            ForEach(game.options.indices, id: \.self) { index in
+                                if (!game.options[index].isFlipped) {
+                                    Text("")
+                                        .foregroundColor(.black)
+                                        .frame(width: 336 - CGFloat((23 * index)), height: 51)
+                                        .background(colors[index % colors.count])
+                                        .cornerRadius(10)
+                                        .padding(.horizontal)
+                                        .padding(.bottom, 10)
+                                } else {
+                                    Text(game.options[index].option)
+                                        .font(Font.custom("SF-Pro-Display-Light", size: 19))
+                                        .foregroundColor(.black)
+                                        .frame(width: 336 - CGFloat((23 * index)), height: 51)
+                                        .background(colors[index % colors.count])
+                                        .cornerRadius(10)
+                                        .padding(.horizontal)
+                                        .padding(.bottom, 10)
+                                }
                             }
                         }
+                        .padding()
                     }
-                    .padding()
-                }
-                
-                ScrollView(.horizontal, showsIndicators: false) {
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(vm.guessesMade, id: \.self) { guess in
+                                Text(guess)
+                                    .foregroundColor(.red.opacity(0.5))
+                                    .padding()
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                    .frame(height: 50)
+                    
                     HStack {
-                        ForEach(vm.guessesMade, id: \.self) { guess in
-                            Text(guess)
-                                .foregroundColor(.red.opacity(0.5))
-                                .padding()
+                        Spacer(minLength: 20)
+                        TextField("Type your guess...           \(vm.current_word_guessing_game?.numberOfGuesses ?? 0) left", text: $currentGuess)
+                            .font(Font.custom("SF-Pro-Display-Light", size: 19))
+                            .keyboardType(.default)
+                            .padding(9)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                    .stroke(Color.gray, lineWidth: 2)
+                            )
+                        Button("Enter") {
+                            if currentGuess != "" {
+                                vm.submitGuess(currentGuess)
+                            }
+                            currentGuess = ""
                         }
-                    }
-                    .padding(.horizontal)
-                }
-                .frame(height: 50)
-                
-                HStack {
-                    Spacer(minLength: 20)
-                    TextField("Type your guess...           \(vm.current_word_guessing_game?.numberOfGuesses ?? 0) left", text: $currentGuess)
                         .font(Font.custom("SF-Pro-Display-Light", size: 19))
-                        .keyboardType(.default)
-                        .padding(9)
-                        .background(
-                            RoundedRectangle(cornerRadius: 10, style: .continuous)
-                                .stroke(Color.gray, lineWidth: 2)
-                        )
-                    Button("Enter") {
-                        if currentGuess != "" {
-                            vm.submitGuess(currentGuess)
-                        }
-                        currentGuess = ""
+                        .foregroundColor(.black)
+                        .frame(minWidth: 0, maxWidth: 71, minHeight: 45)
+                        .background(vm.current_word_guessing_game?.numberOfGuesses ?? 0 > 0 ? Color("EnterButtonColor") : Color.black.opacity(0.1))
+                        .cornerRadius(10)
+                        Spacer(minLength: 20)
                     }
-                    .font(Font.custom("SF-Pro-Display-Light", size: 19))
-                    .foregroundColor(.black)
-                    .frame(minWidth: 0, maxWidth: 71, minHeight: 45)
-                    .background(vm.current_word_guessing_game?.numberOfGuesses ?? 0 > 0 ? Color("EnterButtonColor") : Color.black.opacity(0.1))
-                    .cornerRadius(10)
-                    Spacer(minLength: 20)
+                } else {
+                    Text("No game available")
+                        .padding()
                 }
-            } else {
-                Text("No game available")
-                .padding()
             }
+            .onAppear {
+                vm.create_mock_wg_game()
+            }
+            .navigationBarBackButtonHidden()
         }
-        .onAppear {
-            vm.create_mock_wg_game()
-        }
-        .navigationBarBackButtonHidden()
     }
 }
 
