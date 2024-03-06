d07516edc0af36ea7194444e0a81be43e2991d90
ryan-omeara <ryanomeara12@gmail.com>
2024-03-05 19:57:06 -0500

    Initial changes by Rishi

--- Cultured/Views/Modules/PopCulture/MusicView.swift
+++ Cultured/Views/Modules/PopCulture/MusicView.swift
@@ -1,18 +1,248 @@
 //
+
 //  MusicView.swift
+
 //  Cultured
+
 //
+
 //  Created by Arina Shah on 2/6/24.
+
 //
+
+
 
 import SwiftUI
 
+
+
 struct MusicView: View {
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        VStack{
+
+            Text("Music")
+
+                .frame(maxWidth:325, alignment: .leading)
+
+            Text("Country Name")
+
+                .frame(maxWidth:325, alignment: .leading)
+
+            Spacer()
+
+                .frame(maxHeight: 30)
+
+            Text("Artist Spotlight")
+
+            
+
+            HStack{
+
+                Button {
+
+                    
+
+                } label: {
+
+                    Text("Artist 1")
+
+                    
+
+                        .foregroundStyle(.white)
+
+                        .padding()
+
+                }
+
+                .frame(height: 70)
+
+                .background(Color.cGreen)
+
+                .clipShape(.circle)
+
+                
+
+                
+
+                Button {
+
+                    
+
+                } label: {
+
+                    Text("Artist 2")
+
+                        .foregroundStyle(.white)
+
+                        .padding()
+
+                }
+
+                .frame(height:70
+
+                )
+
+                .background(Color.cGreen)
+
+                .clipShape(.circle)
+
+                Button {
+
+                    
+
+                } label: {
+
+                    Text("Artist 3")
+
+                        .foregroundStyle(.white)
+
+                        .padding()
+
+                }
+
+                .frame(height:70
+
+                )
+
+                .background(Color.cGreen)
+
+                .clipShape(.circle)
+
+                
+
+                
+
+            }
+
+            Spacer()
+
+                .frame(maxHeight: 25)
+
+            Text("Trending Songs")
+
+            VStack{
+
+                Button {
+
+                    
+
+                } label: {
+
+                    HStack{
+
+                        VStack{
+
+                            Text("First song")
+
+                                .foregroundStyle(.white)
+
+                            Text("Artists")
+
+                        }
+
+                        .padding()
+
+                        Image("Landscape")
+
+                    }
+
+                }
+
+                .frame(width: 300)
+
+                .background(Color.red)
+
+                .clipShape(.rect(cornerRadius: 25))
+
+                Button {
+
+                    
+
+                } label: {
+
+                    HStack{
+
+                        VStack{
+
+                            Text("Second song")
+
+                                .foregroundStyle(.white)
+
+                            Text("Artists")
+
+                        }
+
+                        .padding()
+
+                        Image("Landscape")
+
+                    }
+
+                }
+
+                .frame(width:300)
+
+                .background(Color.red)
+
+                .clipShape(.rect(cornerRadius: 25))
+
+                Button {
+
+                    
+
+                } label: {
+
+                    HStack{
+
+                        VStack{
+
+                            Text("Third song")
+
+                                .foregroundStyle(.white)
+
+                            Text("Artists")
+
+                        }
+
+                        .padding()
+
+                        Image("Landscape")
+
+                    }
+
+                }
+
+                .frame(width: 300)
+
+                .background(Color.red)
+
+                .clipShape(.rect(cornerRadius: 25))
+
+            
+
+            }
+
+            
+
+        }
+
+            
+
+        }
+
     }
-}
+
+
+
+
+
+
 
 #Preview {
+
     MusicView()
+
 }
+
