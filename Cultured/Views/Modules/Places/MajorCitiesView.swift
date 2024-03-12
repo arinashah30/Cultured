94db16169831619b76086423f05fe57661749148
Michelle Lee <michlee0912@gmail.com>
2024-03-12 19:32:50 -0400

    MajorCitiesView UI almost complete

--- Cultured/Views/Modules/Places/MajorCitiesView.swift
+++ Cultured/Views/Modules/Places/MajorCitiesView.swift
@@ -14,7 +14,7 @@ struct MajorCitiesView: View {
                 Image("MajorCities")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
-                    .frame(width: 400, height: 470)
+                    .frame(width: 400, height: 200)
                 
                 Button {
                     
@@ -23,52 +23,59 @@ struct MajorCitiesView: View {
                     ZStack {
                         Circle()
                             .frame(width: 50, height: 50)
-                            .padding(.top, 50)
+                            .padding(.top, 20)
                             .padding(.leading, 20)
                             .foregroundColor(Color.white.opacity(0.8))
                         Image("Arrow")
-                            .padding(.top, 50)
+                            .padding(.top, 20)
                             .padding(.leading, 18)
                     }
                     
                 }
                 
             }
-            
-            ZStack (alignment: .topLeading){
-                Rectangle()
-                    .frame(width: 395, height: 380)
-                    .clipShape(.rect(cornerRadius: 40))
-                    .foregroundColor(.white)
-                VStack (alignment: .leading){
-                    Text("Major Cities")
-                        .foregroundColor(.cDarkGray)
-                        .font(Font.custom("Quicksand-SemiBold", size: 32))
-                    Text("Mexico")
-                        .foregroundColor(.cMedGray)
-                    //ForEach (array of regions) { region in Text(region) }
-                    Text("North Mexico")
-                        .font(Font.custom("Quicksand-Medium", size: 24))
-                        .foregroundColor(.cDarkGray)
-                        .padding(.top, 20)
-                    HStack {
-                        VStack {
-                            Image("MapPointer")
+            ScrollView {
+                ZStack (alignment: .topLeading){
+                    Rectangle()
+                        .frame(width: 395, height: 600)
+                        .clipShape(.rect(cornerRadius: 40))
+                        .foregroundColor(.white)
+                    VStack (alignment: .leading){
+                        Text("Major Cities")
+                            .foregroundColor(.cDarkGray)
+                            .font(Font.custom("Quicksand-SemiBold", size: 32))
+                            .padding(.top, 20)
+                        Text("Mexico")
+                            .foregroundColor(.cMedGray)
+                        //ForEach (array of regions) { region in Text(region) }
+                        Text("North Mexico")
+                            .font(Font.custom("Quicksand-Medium", size: 24))
+                            .foregroundColor(.cDarkGray)
+                            .padding(.top, 20)
+                            .padding(.bottom, 10)
+                        HStack (alignment: .top){
+                            VStack {
+                                Image("MapPointer")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 26, height: 33)
+                                Text("San Miguel De Allende")
+                                    .font(Font.custom("Quicksand-Medium", size: 20))
+                                    .foregroundColor(.cDarkGray)
+                                    .padding(.bottom, 5)
+                                Text("In this vibrant city, do this and this activity.")
+                                    .foregroundColor(.cMedGray)
+                            }
+                            .padding(.top, 20)
+                            Image("SanMiguel")
                                 .resizable()
                                 .aspectRatio(contentMode: .fill)
-                                .frame(width: 26, height: 33)
-                            Text("San Miguel De Allende")
-                                .font(Font.custom("Quicksand-Medium", size: 20))
-                                .foregroundColor(.cDarkGray)
-                                .padding(.top, 20)
-                            Text("insert description of the major city, there are these many things to do and see.")
-                                .foregroundColor(.cDarkGray)
+                                .frame(width: 180, height: 180)
+                                .padding(.leading, 10)
                         }
-                        Image("SanMiguel")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 180, height: 180)
                     }
+                    .padding(.leading, 20)
+                    .padding(.trailing, 20)
                 }
             }
         }
