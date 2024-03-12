a68febdb5628612d07def06a9e22103a061cea74
ryan-omeara <ryanomeara12@gmail.com>
2024-03-12 19:32:49 -0400

    updated content, needs reformatting

--- Cultured/Views/Modules/PopCulture/MusicView.swift
+++ Cultured/Views/Modules/PopCulture/MusicView.swift
@@ -1,248 +1,158 @@
-//
-
 //  MusicView.swift
-
 //  Cultured
-
 //
-
 //  Created by Arina Shah on 2/6/24.
-
 //
-
-
-
 import SwiftUI
-
-
-
 struct MusicView: View {
-
     var body: some View {
-
-        VStack{
-
-            Text("Music")
-
-                .frame(maxWidth:325, alignment: .leading)
-
-            Text("Country Name")
-
-                .frame(maxWidth:325, alignment: .leading)
-
-            Spacer()
-
-                .frame(maxHeight: 30)
-
-            Text("Artist Spotlight")
-
-            
-
-            HStack{
-
-                Button {
-
-                    
-
-                } label: {
-
-                    Text("Artist 1")
-
-                    
-
-                        .foregroundStyle(.white)
-
-                        .padding()
-
-                }
-
-                .frame(height: 70)
-
-                .background(Color.cGreen)
-
-                .clipShape(.circle)
-
-                
-
-                
-
-                Button {
-
-                    
-
-                } label: {
-
-                    Text("Artist 2")
-
-                        .foregroundStyle(.white)
-
-                        .padding()
-
-                }
-
-                .frame(height:70
-
-                )
-
-                .background(Color.cGreen)
-
-                .clipShape(.circle)
-
-                Button {
-
-                    
-
-                } label: {
-
-                    Text("Artist 3")
-
-                        .foregroundStyle(.white)
-
-                        .padding()
-
-                }
-
-                .frame(height:70
-
-                )
-
-                .background(Color.cGreen)
-
-                .clipShape(.circle)
-
-                
-
-                
-
-            }
-
-            Spacer()
-
-                .frame(maxHeight: 25)
-
-            Text("Trending Songs")
-
+        ZStack{
             VStack{
-
-                Button {
-
-                    
-
-                } label: {
-
-                    HStack{
-
-                        VStack{
-
-                            Text("First song")
-
-                                .foregroundStyle(.white)
-
-                            Text("Artists")
-
-                        }
-
-                        .padding()
-
-                        Image("Landscape")
-
-                    }
-
-                }
-
-                .frame(width: 300)
-
-                .background(Color.red)
-
-                .clipShape(.rect(cornerRadius: 25))
-
-                Button {
-
-                    
-
-                } label: {
-
-                    HStack{
-
-                        VStack{
-
-                            Text("Second song")
-
-                                .foregroundStyle(.white)
-
-                            Text("Artists")
-
-                        }
-
-                        .padding()
-
-                        Image("Landscape")
-
-                    }
-
-                }
-
-                .frame(width:300)
-
-                .background(Color.red)
-
-                .clipShape(.rect(cornerRadius: 25))
-
-                Button {
-
-                    
-
-                } label: {
-
-                    HStack{
-
-                        VStack{
-
-                            Text("Third song")
-
-                                .foregroundStyle(.white)
-
-                            Text("Artists")
-
-                        }
-
-                        .padding()
-
-                        Image("Landscape")
-
-                    }
-
-                }
-
-                .frame(width: 300)
-
-                .background(Color.red)
-
-                .clipShape(.rect(cornerRadius: 25))
-
-            
-
+                Image("MusicImage")
+                    .resizable()
+                    .frame(width:575, height:323)
+                
+                Spacer()
+                    .frame(height:555)
             }
-
-            
-
+            VStack{
+                Spacer()
+                    .frame(height:290)
+                ScrollView(.vertical) {
+                    VStack{
+                        Spacer()
+                            .frame(height: 20)
+                        Text("Music")
+                            .frame(maxWidth:350, alignment: .leading)
+                            .font(Font.custom("Quicksand-Medium", size: 32))
+                            .foregroundColor(Color(red:252/255, green:64/255, blue:64/255))
+                        
+                        Text("Mexico")
+                            .frame(maxWidth:325, alignment: .leading)
+                            .font(Font.custom("Quicksand-Light", size: 15))
+                        
+                        
+                        Text("Styles")
+                            .font(Font.custom("Quicksand-Medium", size:20))
+                            .frame(maxWidth:350, alignment: .leading)
+                        
+                        Button{
+    
+             } label: {
+        HStack{
+            Image("Mariachi")
+               .resizable()
+                        .frame(width:145, height:185, alignment: .leading)
+               .cornerRadius(14)
+                        VStack{
+                            Text("Mariachi Information")
+                                .font(Font.custom("Quicksand-Regular", size : 20))
+                                      Spacer()
+                                 }
+                             }
+                        Spacer()
+                            .frame(height: 25)
+                        Text("Classics")
+                            .font(Font.custom("Quicksand-Regular", size: 20))
+                            .frame(maxWidth: 340, alignment: .leading)
+                        VStack{
+                            Button {
+                                
+                            } label: {
+                                HStack{
+                                    Image("FirstLoveIcon")
+                                        .resizable()
+                                        .frame(width:58, height:58, alignment: .leading)
+                                        .cornerRadius(5)
+                                    Spacer()
+                                        .frame(width:10)
+                                    VStack{
+                                        Text("First song")
+                                            .foregroundStyle(.black)
+                                        Text("Artists")
+                                    }
+                                    .padding()
+                                    Image("SpotifyIcon")
+                                        .resizable()
+                                        .frame(width:30, height:30)
+                                    Image("AppleMusicIcon")
+                                        .resizable()
+                                        .frame(width:30, height: 30)
+                                }
+                            }
+                            .frame(width: 324, height:76)
+                            .background(Color.cLightGray)
+                            .clipShape(.rect(cornerRadius: 14))
+                            Button {
+                                
+                            } label: {
+                                HStack{
+                                    Image("FirstLoveIcon")
+                                        .resizable()
+                                        .frame(width:58, height:58)
+                                        .cornerRadius(5)
+                                    Spacer()
+                                        .frame(width:10)
+                                    VStack{
+                                        Text("First song")
+                                            .foregroundStyle(.black)
+                                        Text("Artists")
+                                    }
+                                    .padding()
+                                    Image("AppleMusicIcon")
+                                        .resizable()
+                                        .frame(width:30, height:30)
+                                    Image("YTMusicIcon")
+                                        .resizable()
+                                        .frame(width:30, height: 21)
+                                }
+                            }
+                            .frame(width: 324, height:76)
+                            .background(Color.cLightGray)
+                            .clipShape(.rect(cornerRadius: 14))
+                            Button {
+                                
+                            } label: {
+                                HStack{
+                                    Image("FirstLoveIcon")
+                                        .resizable()
+                                        .frame(width:58, height:58)
+                                        .cornerRadius(5)
+                                    Spacer()
+                                        .frame(width:10)
+                                    VStack{
+                                        Text("First song")
+                                            .foregroundStyle(.black)
+                                        Text("Artists")
+                                    }
+                                    .padding()
+                                    Image("SpotifyIcon")
+                                        .resizable()
+                                        .frame(width:30, height:30)
+                                    Image("YTMusicIcon")
+                                        .resizable()
+                                        .frame(width:30, height: 21)
+                                }
+                            }
+                            .frame(width: 324, height:76)
+                            .background(Color.cLightGray)
+                            .clipShape(.rect(cornerRadius: 14))
+                            
+                        }
+                        
+                    }
+                    .frame(width: 400.0, height: 500.0)
+                    .background(Color.white)
+                    .clipShape(.rect(cornerRadius: 50))
+                }
+            }
         }
-
-            
-
         }
-
     }
-
-
-
-
-
-
-
+                       }
 #Preview {
-
     MusicView()
-
 }
+
+
 
