ee460ad7aa6dd28ac68a6bc50784a260ed3cbc3e
Janhavi Purkar <jpurkar3@gatech.edu>
2024-03-13 22:01:47 -0400

    fix results view

--- Cultured/Views/Quizzes_Games/WordGuessing/WordGuessingResultsView.swift
+++ Cultured/Views/Quizzes_Games/WordGuessing/WordGuessingResultsView.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 struct WordGuessingResultsView: View {
     @ObservedObject var vm: WordGuessingViewModel
+    
     var body: some View {
-        RoundedRectangle(cornerRadius: 14.0)
-            .foregroundColor(.white)
-            .frame(width: 304, height: 532)
-        Text("Results")
-            .font(.title)
+        ZStack(alignment: .topLeading) { // Use ZStack to overlay elements
+            RoundedRectangle(cornerRadius: 14.0)
+                .foregroundColor(.white)
+                .frame(width: 304, height: 532)
+            
+            Text("Results")
+                .font(.title)
+                .padding([.top, .leading], 20)
+        }
     }
 }
 
