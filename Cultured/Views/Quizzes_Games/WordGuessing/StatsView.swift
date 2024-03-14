4060629c0aad4718d14f9eb04ebc2375c0825769
shreyasgoyal213 <the.shreyasgoyal@gmail.com>
2024-03-13 23:26:47 -0400

    Created a stats view to display a histogram of game statistics.

---
+++ Cultured/Views/Quizzes_Games/WordGuessing/StatsView.swift
@@ -0,0 +1,47 @@
+//
+//  StatsView.swift
+//  Cultured
+//
+//  Created by Shreyas Goyal on 3/13/24.
+//
+
+import SwiftUI
+
+struct StatsView: View {
+    @ObservedObject var vm: WordGuessingViewModel
+    
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 4) {
+            let adjustedStats = adjustStats(originalStats: vm.stats)
+            ForEach(0..<9, id: \.self) { index in
+                VStack() {
+                    Text(String(vm.stats[index]))
+                        .foregroundColor(Color.black.opacity(0.35))
+                    Rectangle()
+                        .frame(width: 25, height: 200 * CGFloat(adjustedStats[index]))
+                        .foregroundColor(Color.black.opacity(0.15))
+                    Text(String(index + 1))
+                        .foregroundColor(Color.black.opacity(0.5))
+                }
+                
+            }
+        }
+        
+    }
+}
+
+func adjustStats(originalStats: [Int]) -> [Float] {
+    var maxNum: Int = 9
+    if let maxStat = originalStats.max() {
+        maxNum = max(maxStat, 9)
+    }
+    var scaledStats: [Float] = []
+    for stat in originalStats {
+        scaledStats.append(0.1 + (Float(stat) / Float(maxNum)))
+    }
+    return scaledStats
+}
+
+#Preview {
+    StatsView(vm: WordGuessingViewModel())
+}
