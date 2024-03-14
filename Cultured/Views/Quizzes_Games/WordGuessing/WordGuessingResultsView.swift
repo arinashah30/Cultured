5908e1f71d4c1863868d38de6bd1279f660ba215
Janhavi Purkar <jpurkar3@gatech.edu>
2024-03-13 22:17:12 -0400

    added border for visibility of results view.

--- Cultured/Views/Quizzes_Games/WordGuessing/WordGuessingResultsView.swift
+++ Cultured/Views/Quizzes_Games/WordGuessing/WordGuessingResultsView.swift
@@ -9,17 +9,30 @@ import SwiftUI
 
 struct WordGuessingResultsView: View {
     @ObservedObject var vm: WordGuessingViewModel
+    @State private var localHasWon: Bool = true
     
     var body: some View {
-        ZStack(alignment: .topLeading) { // Use ZStack to overlay elements
+        ZStack(alignment: .topLeading) {
             RoundedRectangle(cornerRadius: 14.0)
                 .foregroundColor(.white)
                 .frame(width: 304, height: 532)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 14.0)
+                        .stroke(Color.black, lineWidth: 1)
+                )
             
-            Text("Results")
-                .font(.title)
-                .padding([.top, .leading], 20)
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Results")
+                    .font(Font.custom("Quicksand-Medium", size: 24))
+                Text("You guessed it!")
+                    .font(Font.custom("Quicksand-Medium", size: 20))
+                    .foregroundColor(localHasWon ? Color("WinningText") : .red)
+                Text("Answer: word")
+                    .font(Font.custom("Quicksand-Medium", size: 20))
+            }
+            .padding([.top, .leading], 20)
         }
+
     }
 }
 
