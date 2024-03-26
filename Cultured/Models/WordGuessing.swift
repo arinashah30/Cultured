6f35fd467ac46200947a999000bf4552907fdda4
Connor-1233 <Connor.e.case@gmail.com>
2024-03-26 19:13:54 -0400

    Overloading constructors

--- Cultured/Models/WordGuessing.swift
+++ Cultured/Models/WordGuessing.swift
@@ -15,4 +15,21 @@ struct WordGuessing {
     var flipPoints: Int = 10
     var flipsDone: Int = 0
     var numberOfGuesses: Int = 2
+    
+    
+    init(title: String, options: [OptionTile], answer: String, totalPoints: Int, flipPoints: Int, flipsDone: Int, numberOfGuesses: Int) {
+        self.title = title
+        self.options = options
+        self.answer = answer
+        self.totalPoints = totalPoints
+        self.flipPoints = flipPoints
+        self.flipsDone = flipsDone
+        self.numberOfGuesses = numberOfGuesses
+    }
+    
+    init(title: String, options: [OptionTile], answer: String) {
+        //set values to 0 as default
+        self.init(title: title, options: options, answer: answer, totalPoints: 0, flipPoints: 0, flipsDone: 0, numberOfGuesses: 0)
+        
+    }
 }
