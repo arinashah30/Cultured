62cd185a04d62c156399152316557d4e89f1e406
Arina Shah <101674938+arinashah30@users.noreply.github.com>
2024-03-29 15:49:52 -0400

    Revert "Fixing create model functions"

--- Cultured/Models/Connections.swift
+++ Cultured/Models/Connections.swift
@@ -6,58 +6,116 @@
 //
 
 import Foundation
-import OrderedCollections
 
 struct Connections {
-    var title: String
-    var options: [Option]
+    private(set) var title: String
+    private(set) var categories: [String]
+    private(set) var answerKey: [String : [String]]
+    private(set) var options: [Option]
     var selection: [Option]
-    var answer_key: [String : [Option]]
-    var history: [[String]]
-    var correct_history: OrderedDictionary<String, [Option]>
-    var mistake_history: OrderedDictionary<[Option], Bool>
-    var mistakes_remaining: Int
-    var attempts: Int
-    var one_away: Bool
-    var already_guessed: Bool
+    var history: [[Option]]
     var points: Int
+    var attempts: Int
+    var mistakes_remaining: Int
+    var correct_categories: Int
     
-    init(title: String, answer_key: [String: [String]], history: [[String]]) {
-        self.title = title
-        self.answer_key = [:]
+    init(optionContent: (Int) -> String, optionCategory: (Int) -> String) {
+        title = ""
+        categories = []
+        answerKey = [:]
         selection = []
-        self.history = history
-        mistake_history = [:]
-        correct_history = [:]
-        mistakes_remaining = 4
+        history = []
+        points = 0
         attempts = 0
-        one_away = false
-        already_guessed = false
-        points = 100
+        mistakes_remaining = 4
+        correct_categories = 0
         
         options = []
         
-        for key in answer_key.keys {
-            for value in answer_key[key]! {
-                let option: Option = Option(content: value, category: key)
-                options.append(option)
-                if self.answer_key.keys.contains(key) {
-                    self.answer_key[key]?.append(option)
+        for index in 0..<16 {
+            let content = optionContent(index)
+            let category = optionCategory(index)
+            options.append(Option(id: "\(index + 1)", content: content, category: category))
+        }
+    }
+    
+    init(title: String, categories: [String], answerKey: [String : [String]], options: [Option], selection: [Option], history: [[Option]], points: Int, attempts: Int, mistakes_remaining: Int, correct_categories: Int) {
+            self.title = title
+            self.categories = categories
+            self.answerKey = answerKey
+            self.options = options
+            self.selection = selection
+            self.history = history
+            self.points = points
+            self.attempts = attempts
+            self.mistakes_remaining = mistakes_remaining
+            self.correct_categories = correct_categories
+    }
+    
+    mutating func select(_ option: Option) {
+        let chosenIndex = index(of: option)
+        if chosenIndex >= 0 {
+            if !options[chosenIndex].isSubmitted {
+                if !options[chosenIndex].isSelected {
+                    if selection.count < 4 {
+                        options[chosenIndex].isSelected = true
+                        selection.append(options[chosenIndex])
+                    }
                 } else {
-                    self.answer_key.updateValue([option], forKey: key)
+                    options[chosenIndex].isSelected = false
+                    for index in selection.indices {
+                        if selection[index].content == options[chosenIndex].content {
+                            selection.remove(at: index)
+                            break
+                        }
+                    }
                 }
             }
         }
+    }
+    
+    func index(of option: Option) -> Int {
+        for index in options.indices {
+            if options[index].id == option.id {
+                print(index)
+                return index
+            }
+        }
         
+        return -1
+    }
+    
+    mutating func submit() {
+        if selection.count == 4 {
+            history.append(selection)
+            if selection.allSatisfy({ $0.category == selection[0].category }) {
+                for index in selection.indices {
+                    for index2 in options.indices {
+                        if options[index2].content == selection[index].content {
+                            options[index2].isSubmitted = true
+                            options[index2].isSelected = false
+                            break
+                        }
+                    }
+                }
+                selection = []
+                correct_categories += 1
+            } else {
+                mistakes_remaining -= 1
+            }
+            attempts += 1
+        }
+    }
+    
+    mutating func shuffle() {
         options.shuffle()
     }
     
-    struct Option : Hashable, Comparable {
-        static func < (lhs: Connections.Option, rhs: Connections.Option) -> Bool {
-            return lhs.content < rhs.content
-        }
+    struct Option : Identifiable {
+        var id: String
         
         var isSelected: Bool = false
+        var isSubmitted: Bool = false
         let content: String
         let category: String
     }
