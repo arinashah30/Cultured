5939b7d51528cbac6d802fd3689eb03073b9f4e4
ryan-omeara <ryanomeara12@gmail.com>
2024-04-02 15:43:57 -0400

    Linked music view to pop culture, added back button to music view

--- Cultured/Views/PopCultureSectionView.swift
+++ Cultured/Views/PopCultureSectionView.swift
@@ -11,105 +11,109 @@ struct PopCultureSectionView: View {
     @ObservedObject var vm: ViewModel
 
     var body: some View {
-        ZStack(alignment:.topLeading){
-            Image("PopCulture")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 400, height: 470)
-            
-            BackButton()
+        NavigationStack {
+            ZStack(alignment:.topLeading){
+                Image("PopCulture")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 400, height: 470)
                 
-            VStack {
-                Spacer()
-                ZStack (alignment: .topLeading){
-                    Rectangle()
-                        .frame(width: 395, height: 405)
-                        .clipShape(.rect(cornerRadius: 40))
-                        .foregroundColor(.white)
-                    VStack (alignment: .leading){
-                        Text("Pop Culture")
-                            .foregroundColor(.cDarkGray)
-                            .font(Font.custom("Quicksand-SemiBold", size: 32))
-                        Text("Mexico")
-                            .foregroundColor(.cMedGray)
-                        Text("Categories")
-                            .font(Font.custom("Quicksand-Medium", size: 24))
-                            .foregroundColor(.cDarkGray)
-                            .padding(.top, 20)
-                        HStack (spacing: 13) {
-                            Button {
+                BackButton()
+                
+                VStack {
+                    Spacer()
+                    ZStack (alignment: .topLeading){
+                        Rectangle()
+                            .frame(width: 395, height: 405)
+                            .clipShape(.rect(cornerRadius: 40))
+                            .foregroundColor(.white)
+                        VStack (alignment: .leading){
+                            Text("Pop Culture")
+                                .foregroundColor(.cDarkGray)
+                                .font(Font.custom("Quicksand-SemiBold", size: 32))
+                            Text("Mexico")
+                                .foregroundColor(.cMedGray)
+                            Text("Categories")
+                                .font(Font.custom("Quicksand-Medium", size: 24))
+                                .foregroundColor(.cDarkGray)
+                                .padding(.top, 20)
+                            HStack (spacing: 13) {
+                                NavigationLink {
+                                    MusicView(vm: vm)
+                                        .navigationBarBackButtonHidden(true)
+                                        .toolbar(.hidden, for: .tabBar)
+                                } label: {
+                                    Text("Music")
+                                        .font(.system(size: 20))
+                                        .foregroundColor(.cDarkGray)
+                                        .padding()
+                                }
+                                .frame(maxWidth: 159, maxHeight: 57)
+                                .background(Color.cRed)
+                                .clipShape(.rect(cornerRadius: 14.0))
                                 
-                            } label: {
-                                Text("Music")
-                                    .font(.system(size: 20))
-                                    .foregroundColor(.cDarkGray)
-                                    .padding()
+                                
+                                Button {
+                                    
+                                } label: {
+                                    Text("Dance")
+                                        .font(.system(size: 20))
+                                        .foregroundColor(.cDarkGray)
+                                        .padding()
+                                }
+                                .frame(maxWidth: 159, maxHeight: 57)
+                                .background(Color.cOrange)
+                                .clipShape(.rect(cornerRadius: 14.0))
                             }
-                            .frame(maxWidth: 159, maxHeight: 57)
-                            .background(Color.cRed)
-                            .clipShape(.rect(cornerRadius: 14.0))
+                            .shadow(radius: 4, x: 0, y: 4)
+                            .padding(.bottom, 8)
                             
-                            
-                            Button {
+                            HStack (spacing: 13){
+                                Button {
+                                    
+                                } label: {
+                                    Text("Sports")
+                                        .font(.system(size: 20))
+                                        .foregroundColor(.cDarkGray)
+                                        .padding()
+                                }
+                                .frame(maxWidth: 159, maxHeight: 57)
+                                .background(Color.cGreen)
+                                .clipShape(.rect(cornerRadius: 14.0))
                                 
-                            } label: {
-                                Text("Dance")
-                                    .font(.system(size: 20))
-                                    .foregroundColor(.cDarkGray)
-                                    .padding()
+                                Button {
+                                    
+                                } label: {
+                                    Text("Movies/TV")
+                                        .font(.system(size: 20))
+                                        .foregroundColor(.cDarkGray)
+                                        .padding()
+                                }
+                                .frame(maxWidth: 159, maxHeight: 57)
+                                .background(Color.cBlue)
+                                .clipShape(.rect(cornerRadius: 14.0))
                             }
-                            .frame(maxWidth: 159, maxHeight: 57)
-                            .background(Color.cOrange)
-                            .clipShape(.rect(cornerRadius: 14.0))
-                        }
-                        .shadow(radius: 4, x: 0, y: 4)
-                        .padding(.bottom, 8)
-                        
-                        HStack (spacing: 13){
-                            Button {
-                                
-                            } label: {
-                                Text("Sports")
-                                    .font(.system(size: 20))
-                                    .foregroundColor(.cDarkGray)
-                                    .padding()
+                            .shadow(radius: 4, x: 0, y: 4)
+                            HStack {
+                                Spacer()
+                                Button {} label: {
+                                    Text("Quiz Me!")
+                                        .foregroundColor(.cMedGray)
+                                        .font(.system(size: 20))
+                                }
+                                Spacer()
                             }
-                            .frame(maxWidth: 159, maxHeight: 57)
-                            .background(Color.cGreen)
-                            .clipShape(.rect(cornerRadius: 14.0))
-                            
-                            Button {
-                                
-                            } label: {
-                                Text("Movies/TV")
-                                    .font(.system(size: 20))
-                                    .foregroundColor(.cDarkGray)
-                                    .padding()
-                            }
-                            .frame(maxWidth: 159, maxHeight: 57)
-                            .background(Color.cBlue)
-                            .clipShape(.rect(cornerRadius: 14.0))
-                        }
-                        .shadow(radius: 4, x: 0, y: 4)
-                        HStack {
-                            Spacer()
-                            Button {} label: {
-                                Text("Quiz Me!")
-                                    .foregroundColor(.cMedGray)
-                                    .font(.system(size: 20))
-                            }
-                            Spacer()
+                            .padding(.top, 30)
+                            .padding(.leading, -35)
                         }
                         .padding(.top, 30)
-                        .padding(.leading, -35)
+                        .padding(.leading, 32)
                     }
-                    .padding(.top, 30)
-                    .padding(.leading, 32)
                 }
+                .padding(.leading, 2)
             }
-            .padding(.leading, 2)
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
         }
-        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
     }
 }
 
