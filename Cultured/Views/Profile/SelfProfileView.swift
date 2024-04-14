be836719050cdf2cd32cbc0b7d475f448f7e323e
arinashah30 <ashah634@gatech.edu>
2024-04-14 18:27:08 -0400

    UI changes and merge with main

--- Cultured/Views/Profile/SelfProfileView.swift
+++ Cultured/Views/Profile/SelfProfileView.swift
@@ -26,7 +26,8 @@ struct SelfProfileView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
+            ScrollView {
+                
                 //Settings bar
                 HStack {
                     Spacer()
@@ -59,32 +60,6 @@ struct SelfProfileView: View {
                             .clipShape(.circle)
                     }
                     PhotosPicker("Edit Profile Picture", selection: $avatarItem, matching: .images).foregroundColor(.blue).font(.system(size: 13))
-//                    VStack {
-//                       Spacer()
-//                       HStack {
-//                           Spacer()
-//                           Button {
-//                               isPickerPresented.toggle()
-//                               print(isPickerPresented)
-//                           } label: {
-//                               Circle()
-//                                   .fill(Color.cMedGray)
-//                                   .frame(width: 44, height: 45)
-//                                   .overlay(
-//                                       Image(systemName: "pencil")
-//                                           .resizable()
-//                                           .aspectRatio(contentMode: .fit)
-//                                           .foregroundColor(.white)
-//                                           .padding(10)
-//                                   )
-////                                   .offset(x: -115, y: -33)//this is hardcoded couldnt figure out a better way to do it
-//                           }
-//                           
-//                       }
-//                        if isPickerPresented {
-//                            PhotosPicker("Edit Profile Picture", selection: $avatarItem, matching: .images)
-//                        }
-//                    }
                 }
                 .onChange(of: avatarItem) {
                     Task {
@@ -104,99 +79,49 @@ struct SelfProfileView: View {
                     print("avim : \(avatarImage)")
                     avatarImage = URL(string: vm.current_user!.profilePicture)
                 }
-                Spacer()
+                Spacer(minLength: 20)
                 
-                VStack{
-                    Text("\(vm.current_user?.name ?? "")")
-                        .font(Font.custom("Quicksand-Semibold", size: 32))
-                        .foregroundColor(.cDarkGray)
-                    
-                    Text("\(vm.current_user?.id ?? "")")
-                        .font(.system(size: 20))
-                        .foregroundColor(.cMedGray)
-                        .padding()
-                }
-            }
-            
-            //Prof pic/placeholder with edit button
-            ZStack{
-                Image("BlankUser")
-                    .resizable()
-                    .frame(width: 156, height: 156)
-                    .background(Color.cLightGray)
-                    .clipShape(Circle())
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Circle()
-                            .fill(Color.cMedGray)
-                            .frame(width: 44, height: 45)
-                            .overlay(
-                                Image(systemName: "pencil")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(.white)
-                                    .padding(10)
-                            )
-                            .offset(x: -115, y: -33)//this is hardcoded couldnt figure out a better way to do it
-                    }
-                }
-            }
-            VStack{
-                Text("\(vm.current_user?.name ?? "No user")")
+                
+                Text("\(vm.current_user?.name ?? "No Username")")
                     .font(Font.custom("Quicksand-Semibold", size: 32))
                     .foregroundColor(.cDarkGray)
-                
-                Text("\(vm.current_user?.id ?? "No user")")
+                Text("\(vm.current_user?.id ?? "No id")")
                     .font(.system(size: 20))
                     .foregroundColor(.cMedGray)
+                
+                
+                Text("My Challenges")
+                    .font(Font.custom("Quicksand", size:24))
+                    .foregroundColor(.cDarkGray)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .multilineTextAlignment(.leading)
+                    .padding([.top, .leading], 15)
+                
+                Button(action: {
+                    self.showFullMap.toggle()
+                }, label: {
+                    MapView(vm: vm, showFullMap: $showFullMap, completedCountries: $completedCountries)
+                        .frame(width:354 ,height: 175)
+                        .cornerRadius(20)
+                        .padding(.bottom, 10)
+                })
+                .fullScreenCover(isPresented: $showFullMap, content: {
+                    MapView(vm: vm, showFullMap: $showFullMap, completedCountries: $completedCountries)
+                })
+                Spacer()
+                ChallengeView(country: vm.current_user?.country ?? "Mexico", status: "In Progress")
+                ForEach(completedCountries, id: \.self) { country in
+                    ChallengeView(country: country, status: "Completed")
+                }
+                
+                
             }
-            Text("My Challenges")
-                .font(Font.custom("Quicksand", size:24))
-                .foregroundColor(.cDarkGray)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .multilineTextAlignment(.leading)
-                .padding([.top, .leading], 15)
-            
-            Button(action: {
-                self.showFullMap.toggle()
-            }, label: {
-                MapView(vm: vm, showFullMap: $showFullMap, completedCountries: $completedCountries)
-                    .frame(width:354 ,height: 175)
-                    .cornerRadius(20)
-                    .padding(.bottom, 10)
-            })
-            .fullScreenCover(isPresented: $showFullMap, content: {
-                MapView(vm: vm, showFullMap: $showFullMap, completedCountries: $completedCountries)
-            })
-            Spacer()
-            ChallengeView(country: vm.current_user?.country ?? "Mexico", status: "In Progress")
-            ForEach(completedCountries, id: \.self) { country in
-                ChallengeView(country: country, status: "Completed")
-            }
-            
-            
-            
         }.onAppear {
             vm.getCompletedCountries(userID: vm.current_user?.id ?? "") { countries in
                 completedCountries = countries
             }
         }
     }
-    
-//        init(vm: ViewModel, showFullMap: Bool = false) {
-//            self.vm = vm
-//            self.showFullMap = showFullMap
-//            self.completedCountries = []
-//            var donecountries: [String] = []
-//            self.vm.getCompletedCountries(userID: vm.current_user?.id ?? "") { countries in
-//                donecountries = countries
-//                print("DONE COUNTRIES \(donecountries)")
-//            }
-//            self.completedCountries = donecountries
-//            print("COMPLETEDCOUNTRIES: \(completedCountries)")
-//        }
 }
 
 struct ChallengeView: View {
