a9119449d2f1c6a8f49a7fb56a9a638be0cd8373
ryan-omeara <ryanomeara12@gmail.com>
2024-04-16 17:30:37 -0400

    Section view sizing + image fetching

--- Cultured/Views/CustomsSectionView.swift
+++ Cultured/Views/CustomsSectionView.swift
@@ -9,37 +9,44 @@ import SwiftUI
 
 struct CustomsSectionView: View {
     @ObservedObject var vm: ViewModel
-    
+    @State var uiImage: UIImage? = nil
+
     var body: some View {
         NavigationStack {
-            ZStack (alignment: .topLeading){
-                Image("Customs")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 400, height: 470)
-                    .offset(y:50)
+            ZStack(alignment:.topLeading){
+                if let uiImage = uiImage {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: screenWidth, height: screenHeight * 0.7)
+                        .ignoresSafeArea()
+
+                } else {
+                    ProgressView()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: screenWidth/2, height: screenHeight/2)
+                        .offset(y:screenHeight * -0.2)
+                }
                 
                 BackButton()
+                    .offset(x:10,y:30)
                 
-                
-                VStack {
-                    Spacer()
-                    ZStack (alignment: .topLeading){
-                        Rectangle()
-                            .frame(width: screenWidth, height: screenHeight * 0.4)
-                            .clipShape(.rect(cornerRadius: 40))
-                            .foregroundColor(.cPopover)
-                        VStack (alignment: .leading){
+                VStack (alignment:.leading){
                             Text("Customs")
                                 .foregroundColor(.cDarkGray)
                                 .font(Font.custom("Quicksand-SemiBold", size: 32))
+                                .padding(.leading,32)
+                                .padding(.top,30)
                             Text(vm.current_user?.country ?? "Mexico")
                                 .foregroundColor(.cMedGray)
+                                .padding(.leading, 32)
                             Text("Categories")
                                 .font(Font.custom("Quicksand-Medium", size: 24))
                                 .foregroundColor(.cDarkGray)
                                 .padding(.top, 20)
-                            HStack (spacing: 13){
+                                .padding(.leading,32)
+                                .padding(.bottom,15)
+                            HStack (spacing: 13) {
                                 NavigationLink {
                                     TraditionsView(vm:vm)
                                         .navigationBarBackButtonHidden(true)
@@ -54,22 +61,7 @@ struct CustomsSectionView: View {
                                 .background(Color.cRed)
                                 .clipShape(.rect(cornerRadius: 14.0))
                                 
-//                                NavigationLink {
-//                                    
-//                                } label: {
-//                                    Text("Holiday")
-//                                        .font(.system(size: 20))
-//                                        .foregroundColor(.cDarkGrayConstant)
-//                                        .padding()
-//                                }
-//                                .frame(maxWidth: 159, maxHeight: 57)
-//                                .background(Color.cOrange)
-//                                .clipShape(.rect(cornerRadius: 14.0))
-//                            }
-//                            .shadow(radius: 4, x: 0, y: 4)
-//                            .padding(.bottom, 7)
-                            
-//                            HStack (spacing: 13){
+                                
                                 NavigationLink {
                                     EtiquetteView(vm: vm)
                                         .navigationBarBackButtonHidden(true)
@@ -85,27 +77,39 @@ struct CustomsSectionView: View {
                                 .clipShape(.rect(cornerRadius: 14.0))
                             }
                             .shadow(radius: 4, x: 0, y: 4)
+                            .padding(.bottom, 8)
+                            .frame(width:screenWidth)
+                          
                             HStack {
-                                Spacer()
                                 Button {} label: {
                                     Text("Quiz Me!")
                                         .foregroundColor(.cMedGray)
                                         .font(.system(size: 20))
                                 }
-                                Spacer()
                             }
                             .padding(.top, 30)
-                            .padding(.leading, -35)
-                        }
-                        .padding(.top, 30)
-                        .padding(.leading, 32)
-                    }
+                            .frame(width: screenWidth)
+
+                    Spacer()
                 }
-                .padding(.leading, 3)
+                .frame(width: screenWidth, height: screenHeight)
+                .background(.cPopover)
+                .clipShape(.rect(cornerRadius: 40))
+                .offset(y:screenHeight * 0.6)
+
+
             }
-            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+            .frame(width: screenWidth, height: screenHeight)
+        }
+        .onAppear {
+            vm.getImage(imageName: "\(vm.get_current_country().lowercased())_customs_home") { image in
+                uiImage = image
         }
     }
+}
+
+
+
 }
 
 #Preview {
--- Cultured/Views/FoodCategorySectionView.swift
+++ Cultured/Views/FoodCategorySectionView.swift
@@ -9,37 +9,45 @@ import SwiftUI
 
 struct FoodCategorySectionView: View {
     @ObservedObject var vm: ViewModel
-    
+    @State var uiImage: UIImage? = nil
+
     var body: some View {
-        NavigationStack{
-            ZStack (alignment: .topLeading){
-                Image("FoodCategory")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 400, height: 470)
+        NavigationStack {
+            ZStack(alignment:.topLeading){
+                if let uiImage = uiImage {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: screenWidth, height: screenHeight * 0.8)
+                        .ignoresSafeArea()
+                } else {
+                    ProgressView()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: screenWidth/2, height: screenHeight/2)
+                        .offset(y:screenHeight * -0.2)
+                }
                 
                 BackButton()
+                    .offset(x:10,y:30)
                 
-                VStack{
-                    Spacer()
-                    ZStack (alignment: .topLeading){
-                        Rectangle()
-                            .frame(width: UIScreen.main.bounds.width, height: 350)
-                            .clipShape(.rect(cornerRadius: 40))
-                            .foregroundColor(.cPopover)
-                        VStack (alignment: .leading){
+                VStack (alignment:.leading){
                             Text("Food")
                                 .foregroundColor(.cDarkGray)
                                 .font(Font.custom("Quicksand-SemiBold", size: 32))
+                                .padding(.leading,32)
+                                .padding(.top,30)
                             Text(vm.current_user?.country ?? "Mexico")
                                 .foregroundColor(.cMedGray)
+                                .padding(.leading, 32)
                             Text("Categories")
                                 .font(Font.custom("Quicksand-Medium", size: 24))
                                 .foregroundColor(.cDarkGray)
                                 .padding(.top, 20)
-                            HStack {
+                                .padding(.leading,32)
+                                .padding(.bottom,15)
+                            HStack (spacing: 13) {
                                 NavigationLink {
-                                    FoodView(vm:vm)
+                                    FoodView(vm: vm)
                                         .navigationBarBackButtonHidden(true)
                                         .toolbar(.hidden, for: .tabBar)
                                 } label: {
@@ -52,8 +60,9 @@ struct FoodCategorySectionView: View {
                                 .background(Color.cRed)
                                 .clipShape(.rect(cornerRadius: 14.0))
                                 
+                                
                                 NavigationLink {
-                                    DrinkView(vm:vm)
+                                    DrinkView(vm: vm)
                                         .navigationBarBackButtonHidden(true)
                                         .toolbar(.hidden, for: .tabBar)
                                 } label: {
@@ -67,7 +76,9 @@ struct FoodCategorySectionView: View {
                                 .clipShape(.rect(cornerRadius: 14.0))
                             }
                             .shadow(radius: 4, x: 0, y: 4)
-                            
+                            .padding(.bottom, 8)
+                            .frame(width:screenWidth)
+         
                             HStack {
                                 Spacer()
                                 Button {} label: {
@@ -78,18 +89,27 @@ struct FoodCategorySectionView: View {
                                 Spacer()
                             }
                             .padding(.top, 30)
-                            .padding(.leading, -35)
-                            
-                        }
-                        .padding(.top, 30)
-                        .padding(.leading, 32)
-                    }
-                    .padding(.leading, 7)
+                            .frame(width: screenWidth)
+
+                    Spacer()
                 }
-                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .frame(width: screenWidth, height: screenHeight)
+                .background(.cPopover)
+                .clipShape(.rect(cornerRadius: 40))
+                .offset(y:screenHeight * 0.6)
+
             }
+            .frame(width: screenWidth, height: screenHeight)
+        }
+        .onAppear {
+            vm.getImage(imageName: "\(vm.get_current_country().lowercased())_food_home") { image in
+                uiImage = image
         }
     }
+}
+
+
+
 }
 
 #Preview {
