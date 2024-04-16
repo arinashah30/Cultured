f6d5608be3e1971b8bf54b6b208057297b0f67c8
jifonthespoon <austin.huguenard@gmail.com>
2024-04-16 18:36:40 -0400

    Fix places view

--- Cultured/Views/PlacesSectionView.swift
+++ Cultured/Views/PlacesSectionView.swift
@@ -30,84 +30,77 @@ struct PlacesSectionView: View {
                 BackButton()
                     .offset(x:10,y:30)
                 
-                VStack{
-                    Spacer()
-                    ZStack (alignment: .topLeading){
-                        Rectangle()
-                            .frame(width: UIScreen.main.bounds.width, height: 350)
-                            .clipShape(.rect(cornerRadius: 40))
-                            .foregroundColor(.cPopover)
-                        VStack (alignment: .leading){
-                            Text("Places")
-                                .foregroundColor(.cDarkGray)
-                                .font(Font.custom("Quicksand-SemiBold", size: 32))
-                                .padding(.leading,32)
-                                .padding(.top,30)
-                            Text(vm.current_user?.country ?? "Mexico")
-                                .foregroundColor(.cMedGray)
-                                .padding(.leading, 32)
-                            Text("Categories")
-                                .font(Font.custom("Quicksand-Medium", size: 24))
-                                .foregroundColor(.cDarkGray)
-                                .padding(.top, 20)
-                            HStack {
-                                NavigationLink {
-                                    LandmarksView(vm:vm)
-                                        .navigationBarBackButtonHidden(true)
-                                        .toolbar(.hidden, for: .tabBar)
-                                } label: {
-                                    Text("Landmarks")
-                                        .font(.system(size: 20))
-                                        .foregroundColor(.cDarkGrayConstant)
-                                        .padding()
-                                }
-                                .frame(maxWidth: 159, maxHeight: 57)
-                                .background(Color.cRed)
-                                .clipShape(.rect(cornerRadius: 14.0))
-                                
-                                NavigationLink {
-                                    LandmarksView(vm:vm)
-                                        .navigationBarBackButtonHidden(true)
-                                        .toolbar(.hidden, for: .tabBar)
-                                } label: {
-                                    Text("Major Cities")
-                                        .font(.system(size: 20))
-                                        .foregroundColor(.cDarkGrayConstant)
-                                        .padding()
-                                }
-                                .frame(maxWidth: 159, maxHeight: 57)
-                                .background(Color.cOrange)
-                                .clipShape(.rect(cornerRadius: 14.0))
-                            }
-                            .shadow(radius: 4, x: 0, y: 4)
-                            .padding(.bottom, 8)
-                            .frame(width:screenWidth)
-                            
-                            HStack {
-                                Button {} label: {
-                                    Text("Quiz Me!")
-                                        .foregroundColor(.cMedGray)
-                                        .font(.system(size: 20))
-                                }
-                            }
-                            .padding(.top, 30)
-                            .frame(width: screenWidth)
-                            
-                            Spacer()
+                VStack (alignment:.leading){
+                    Text("Places")
+                        .foregroundColor(.cDarkGray)
+                        .font(Font.custom("Quicksand-SemiBold", size: 32))
+                        .padding(.leading,32)
+                        .padding(.top,30)
+                    Text(vm.current_user?.country ?? "Mexico")
+                        .foregroundColor(.cMedGray)
+                        .padding(.leading, 32)
+                    Text("Categories")
+                        .font(Font.custom("Quicksand-Medium", size: 24))
+                        .foregroundColor(.cDarkGray)
+                        .padding(.top, 20)
+                        .padding(.leading,32)
+                        .padding(.bottom,15)
+                    HStack (spacing: 13) {
+                        NavigationLink {
+                            LandmarksView(vm: vm)
+                                .navigationBarBackButtonHidden(true)
+                                .toolbar(.hidden, for: .tabBar)
+                        } label: {
+                            Text("Landmarks")
+                                .font(.system(size: 20))
+                                .foregroundColor(.cDarkGrayConstant)
+                                .padding()
                         }
-                        .frame(width: screenWidth, height: screenHeight)
-                        .background(.cPopover)
-                        .clipShape(.rect(cornerRadius: 40))
-                        .offset(y:screenHeight * 0.6)
+                        .frame(maxWidth: 159, maxHeight: 57)
+                        .background(Color.cRed)
+                        .clipShape(.rect(cornerRadius: 14.0))
                         
                         
+                        NavigationLink {
+                            LandmarksView(vm: vm)
+                                .navigationBarBackButtonHidden(true)
+                                .toolbar(.hidden, for: .tabBar)
+                        } label: {
+                            Text("Major Cities")
+                                .font(.system(size: 20))
+                                .foregroundColor(.cDarkGrayConstant)
+                                .padding()
+                        }
+                        .frame(maxWidth: 159, maxHeight: 57)
+                        .background(Color.cOrange)
+                        .clipShape(.rect(cornerRadius: 14.0))
                     }
-                    .frame(width: screenWidth, height: screenHeight)
+                    .shadow(radius: 4, x: 0, y: 4)
+                    .padding(.bottom, 8)
+                    .frame(width:screenWidth)
+                    
+                    HStack {
+                        Spacer()
+                        Button {} label: {
+                            Text("Quiz Me!")
+                                .foregroundColor(.cMedGray)
+                                .font(.system(size: 20))
+                        }
+                        Spacer()
+                    }
+                    .padding(.top, 30)
+                    .frame(width: screenWidth)
+                    
+                    Spacer()
                 }
-                .onAppear {
-                    vm.getImage(imageName: "\(vm.get_current_country().lowercased())_places_home") { image in
-                        uiImage = image
-                    }
+                .frame(width: screenWidth, height: screenHeight)
+                .background(.cPopover)
+                .clipShape(.rect(cornerRadius: 40))
+                .offset(y:screenHeight * 0.6)
+            }
+            .onAppear {
+                vm.getImage(imageName: "\(vm.get_current_country().lowercased())_places_home") { image in
+                    uiImage = image
                 }
             }
         }
