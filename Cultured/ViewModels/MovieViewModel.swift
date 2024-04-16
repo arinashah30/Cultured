357d931bdde0091f4a225d95ea5365d37c61b247
arinashah30 <ashah634@gatech.edu>
2024-04-15 22:19:56 -0400

    broken working version

--- Cultured/ViewModels/MovieViewModel.swift
+++ Cultured/ViewModels/MovieViewModel.swift
@@ -8,22 +8,39 @@
 import Foundation
 
 class MovieViewModel: ObservableObject {
-    let movieNames: [String] = ["Godzilla x Kong: The New Empire", "Kung Fu Panda 4", "SPY x FAMILY CÓDIGO: Blanco", "Un Actor Malo", "Duna Parte Dos", "La Primera Profecía"]
-    let actorNames: [String] = ["Salma Hayek", "Ricardo Montalban", "Gael Garcia Bernal", "Kate del Castillo", "Ana de la Reguera", "Adriana Barraza"]
-    @Published var movies: [MovieItem] = []
+    let theaterMovieNames: [String] = ["Godzilla x Kong: The New Empire", "Kung Fu Panda 4", "SPY x FAMILY CÓDIGO: Blanco", "Un Actor Malo", "Duna Parte Dos", "La Primera Profecía"]
+    let actorNames: [String] //= ["Salma Hayek", "Ricardo Montalban", "Gael Garcia Bernal", "Kate del Castillo", "Ana de la Reguera", "Adriana Barraza"]
+    let classicMovieNames: [String]
+    @Published var theatermovies: [MovieItem] = []
+    @Published var classicmovies: [MovieItem] = []
     @Published var actors: [ActorItem] = []
     static let apiKey = "b7bbe48479f735dcf3fa7ad6bc9cbd1c"
-//    static let token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2JiZTQ4NDc5ZjczNWRjZjNmYTdhZDZiYzljYmQxYyIsInN1YiI6IjY2MDcxM2QyYTZkZGNiMDE2MzQ0ZjM3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oWo37m4dRCfuROD9J_1ngTG9jKLmMoKduXPNOjRLrhg"
+    
+    init(actorNames: [String], classicMovieNames: [String]) {
+        self.actorNames = actorNames
+        self.classicMovieNames = classicMovieNames
+    }
+    
  func loadMovies() {
         Task {
                 do{
-                    for name in movieNames {
+                    for name in theaterMovieNames {
                         let url = URL(string: "https://api.themoviedb.org/3/search/movie?query=\(name.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)&include_adult=false&language=es-mx&page=1&region=mx&api_key=\(MovieViewModel.apiKey)")!
                         let (data, _) = try await URLSession.shared.data(from: url)
                         let movieResults = try? JSONDecoder().decode(MovieResults.self, from: data)
                         if let movieResults {
                             if (!movieResults.results.isEmpty) {
-                                movies.append(movieResults.results[0])
+                                theatermovies.append(movieResults.results[0])
+                            }
+                        }
+                    }
+                    for name in classicMovieNames {
+                        let url = URL(string: "https://api.themoviedb.org/3/search/movie?query=\(name.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)&include_adult=false&language=es-mx&page=1&region=mx&api_key=\(MovieViewModel.apiKey)")!
+                        let (data, _) = try await URLSession.shared.data(from: url)
+                        let movieResults = try? JSONDecoder().decode(MovieResults.self, from: data)
+                        if let movieResults {
+                            if (!movieResults.results.isEmpty) {
+                                classicmovies.append(movieResults.results[0])
                             }
                         }
                     }
